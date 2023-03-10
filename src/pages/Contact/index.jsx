ab09da76470308983035e81b3be9d8f2e8217ac9
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-03-10 18:57:25 +0100

    ignore case fix for git

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import { updateHead } from "../../utilities/updateHead"
+
+function ContactPage(){
+  updateHead("Contact Us", "Contact TechBeauty with your queries, concerns and suggestions.")
+  return(
+    <main>
+      <h1>Contact Page</h1>
+
+    </main>
+  )
+}
+
+export default ContactPage
